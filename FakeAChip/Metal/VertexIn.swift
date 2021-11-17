41c52ea918ff0dade19716cdcc0318ff4e7a7c94
Mike Hall <mike@willowrod.com>
2021-11-17 09:12:17 +0000

    Update to make all classes available to Unit Tests

--- FakeAChip/Metal/VertexIn.swift
+++
@@ -1,8 +0,0 @@
-//
-//  VertexIn.swift
-//  VertexIn
-//
-//  Created by Mike Hall on 21/09/2021.
-//
-
-import Foundation
---
+++ FakeAChip/Source/Processor/Z80/RawZ80Processor.swift
@@ -0,0 +1,8 @@
+//
+//  RawZ80Processor.swift
+//  FakeAChip
+//
+//  Created by Mike Hall on 17/11/2021.
+//
+
+import Foundation
